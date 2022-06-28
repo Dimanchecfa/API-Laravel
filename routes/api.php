b64abfb8696b7b3d2058b88cdc02b4d7aa093567
Dimanchecfa <dimanchenamo@gmail.com>
2022-06-29 01:34:14 +0200

    mon API etudiants

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,9 @@ use App\Http\Controllers\StudentController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) 
+{
+
     return $request->user();
 });
 
